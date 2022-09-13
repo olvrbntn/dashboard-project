9d54df7b75f972784043cfe28f3becfb1120c57d
Oliver Javier <75331630+olvrbntn@users.noreply.github.com>
2022-09-12 17:31:32 -0700

    first commit

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '85632',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '85633',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85634',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85635',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85636',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
